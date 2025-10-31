898a8851561635f60c7f02199e65f94545ad0b97
Prince Kothari <197196645+princekotharii@users.noreply.github.com>
2025-10-31 16:47:21 +0530

    Changed the name of index.js to Server.js

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import { connectDb } from './config/db.js'
+import userRouter from './routers/routes.js'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser)
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}))
+
+app.use('/api',userRouter)
+
+connectDb().
+then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('This is Port' , process.env.PORT );
+    })
+})
